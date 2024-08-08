3f624a72a7405192448bfbd827795e81a9194709
mahongyi <madongxu001@ke.com>
2024-08-08 20:34:01 +0800

    init

---
+++ src/main/java/com/springboot/code/inner_project/InnerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.code.inner_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/code/inner_project/InnerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.code.inner_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
